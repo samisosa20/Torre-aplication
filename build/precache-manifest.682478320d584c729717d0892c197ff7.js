1c3b3b20f9fc58ecdad353c58b4b415828110795
Sammy <1449@holbertonschool.com>
2020-10-15 16:32:25 -0500

    build directory fix

---
+++ build/precache-manifest.682478320d584c729717d0892c197ff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f13b28576271ebc98c96196798cea9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb55f8c4b6085aa59ecc",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9c23928eca9e0072bbb5",
+    "url": "/static/css/main.4f530b61.chunk.css"
+  },
+  {
+    "revision": "cb55f8c4b6085aa59ecc",
+    "url": "/static/js/2.411b87a8.chunk.js"
+  },
+  {
+    "revision": "caacfaae96ba8b2d6a270a80b7b8b4cb",
+    "url": "/static/js/2.411b87a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c23928eca9e0072bbb5",
+    "url": "/static/js/main.91462e9a.chunk.js"
+  },
+  {
+    "revision": "f1aafb948fac413c7e0a",
+    "url": "/static/js/runtime-main.9e15da7e.js"
+  }
+]);
