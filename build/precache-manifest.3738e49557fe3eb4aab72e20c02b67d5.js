712fcd33806007c8d2792fd95f0e298e70414bce
Sammy <1449@holbertonschool.com>
2020-10-15 14:49:37 -0500

    fix css selected nav and upload build directory

---
+++ build/precache-manifest.3738e49557fe3eb4aab72e20c02b67d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0b8bf01e0f3215ccbb0053ac625d69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a91b71bbaefb1dfcd8fe",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "63edecadff120891b8ca",
+    "url": "/static/css/main.d012a274.chunk.css"
+  },
+  {
+    "revision": "a91b71bbaefb1dfcd8fe",
+    "url": "/static/js/2.861940b4.chunk.js"
+  },
+  {
+    "revision": "caacfaae96ba8b2d6a270a80b7b8b4cb",
+    "url": "/static/js/2.861940b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63edecadff120891b8ca",
+    "url": "/static/js/main.d4c7779a.chunk.js"
+  },
+  {
+    "revision": "f1aafb948fac413c7e0a",
+    "url": "/static/js/runtime-main.9e15da7e.js"
+  }
+]);
